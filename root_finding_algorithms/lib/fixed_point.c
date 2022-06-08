156649cb3f33cb7c20f1ee11305971e8dfd005bc
jonatasilva <jonatas.iw@gmail.com>
2022-06-08 18:12:11 +0200

    adjust comments

--- root_finding_algorithms/lib/fixed_point.c
+++ root_finding_algorithms/lib/fixed_point.c
@@ -1,3 +1,17 @@
+/*
+ * Module Name: fixed_point
+ *
+ * Description: Implements the Fixed point method described in
+ *    the manuscript 
+ *
+ *    A. Alves, J.O.L. Silva, L.C. Matioli, P.S.M. Santos, S.S. Souza. 
+ *    "A fixed-point algorithm for solving quadratic convex separable 
+ *     knapsack problems".
+ *
+ *
+ * Copyright: Jonatas O. L. da Silva <jonatas.iw@gmail.com> 2022.
+ */
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -53,6 +67,37 @@ void free_cqk_problem(cqk_problem *restrict p) {
     p->up = NULL;
 }
 
+/********** Fixed point method Separable Quadratic Knapsack problem
+            described in the manuscript
+
+    A. Alves, J.O.L. Silva, L.C. Matioli, P.S.M. Santos, S.S. Souza. 
+    "A fixed-point algorithm for solving quadratic convex separable 
+    knapsack problems".
+
+**********/ 
+
+/*
+ * Function Name: initial_multiplier_fixed_point
+ *
+ * Description: Estimate an initial multiplier from an approximate
+ *     solution.
+ *
+ * Input:
+ *     cqk_problem *restrict p: description of the cont. quad. knapsack.
+ *
+ * Output:
+ *      double *restrict abp: terms resultant of the operation (a*b)/d.
+ *      double *restrict bbp: terms resultant of the operation (b*b)/d.
+ *      double *restrict lr: terms resultant of the operation (a-d*low)/b.
+ *      double *restrict ur: terms resultant of the operation (a-d*up)/b.
+ *      double *restrict bl: terms resultant of the operation b*low.
+ *      double *restrict bu: terms resultant of the operation b*up.
+ * 
+ * Return value: the desired estimate.
+ * 
+ * Obs: The multiplier is computed as suggested by the variable fixing
+ * methods.
+ */
 double initial_multiplier_fixed_point(cqk_problem *restrict p, 
                                 double *restrict abp,
                                 double *restrict bbp,
@@ -78,6 +123,24 @@ double initial_multiplier_fixed_point(cqk_problem *restrict p,
     return lambda;
 }
 
+/*
+ * Function Name: xis_quad
+ *
+ * Description: Compute sum of the term bl, bu, abp, bbp presented
+ *              in its respectives sets necessary to calculate the new multiplier.
+ *
+ * Input:
+ *     cqk_problem *restrict p: description of the cont. quad. knapsack.
+ *     double lambda: current multiplier.     
+ *     double *restrict lr: terms contained the upper bounds for the multiplier.
+ *     double *restrict ur: terms contained the lower bounds for the multiplier. 
+ *
+ * Output:
+ *      double *Sbl: sum of terms bl presented in set I_l.
+ *      double *Sbu: sum of terms bu presented in set I_u.
+ *      double *Sbap: sum of terms abp presented in set I_eq.
+ *      double *Sbbp: sum of terms bbp presented in set I_eq.
+ */
 void xis_quad(cqk_problem *restrict p, double lambda,
                 double *restrict lr,double *restrict ur,
                 double *restrict bl,double *restrict bu,
@@ -105,6 +168,21 @@ void xis_quad(cqk_problem *restrict p, double lambda,
     }
 }
 
+/*
+ * Function Name: mount_x
+ *
+ * Description: Mount the final result x based on the final multiplier and
+ *              box constraints.
+ *
+ * Input:
+ *      cqk_problem *restrict p: description of the cont. quad. knapsack.
+ *      double lambda: final multiplier.     
+ *      double *restrict lr: terms contained the upper bounds for the multiplier.
+ *      double *restrict ur: terms contained the lower bounds for the multiplier.
+ *
+ * Output:
+ *     double *x: solution obtained.
+ */
 void mount_x(cqk_problem *restrict p, double *restrict x, double lambda,
             double *restrict lr,double *restrict ur){
 
@@ -123,6 +201,21 @@ void mount_x(cqk_problem *restrict p, double *restrict x, double lambda,
     }
 }
 
+/*
+ * Function Name: fixed_point
+ *
+ * Description: Fixedpoint method for the sep. quad. knapsack problem.
+ *
+ * Input:
+ *     cqk_problem *restrict p: the problem description.
+ * 
+ * Output:
+ *     double *x: solution obtained.
+ * 
+ * Return value: the number of iterations required to compute the
+ *     solution, -1 in case of failure, -2 in case of infeasible
+ *     problem.
+ */
 int fixed_point(cqk_problem *restrict p, double *x) {
 
     unsigned n_iters = 1;
